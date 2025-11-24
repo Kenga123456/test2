542832594a06612d6cfa344a59fb26d728db6746
Kenga123456 <Kenga123456@outlook.com>
2025-11-24 17:28:00 +0100

    fikset faq slik at teksten viser bedre på små skjermer, fikset meny slik at den blir mindre i høyden når man scroller

--- meny.js
+++ meny.js
@@ -4,3 +4,14 @@ const menu = document.querySelector(".menu");
 menuCollapsed.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
 });
+
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    nav.classList.add("shrink");
+    menu.classList.add("shrink");
+  } else {
+    nav.classList.remove("shrink");
+    menu.classList.remove("shrink");
+  }
+});
